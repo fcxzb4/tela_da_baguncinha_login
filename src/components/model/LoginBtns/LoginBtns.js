163cd84e0d5028f806d9248c29b95f39f0ea3d8a
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-05-29 21:56:27 -0300

    dad

--- src/components/model/LoginBtns/LoginBtns.js
+++ src/components/model/LoginBtns/LoginBtns.js
@@ -1,16 +1,18 @@
 import styles from "./LoginBtns.module.scss"
 import Enterprise from "../../ui/EnterpriseBtn/EnterpriseBtn"
-import {faInstagram , faGoogle , faDiscord} from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faGoogle, faDiscord } from "@fortawesome/free-brands-svg-icons";
 import ButtonText from "../../ui/buttons_text/ButtonText";
 
-function Loginbtns({tryLogin}) {
+function Loginbtns({ tryLogin }) {
     return <>
         <div className={styles.LoginBtns}>
+                <span className={styles.glitch}></span>
+                <span className={styles.glow}></span>
             <ButtonText onClick={tryLogin}>ENTRAR</ButtonText>
             <div className={styles.icon}>
-            <Enterprise icon={faInstagram } />
-            <Enterprise icon={faGoogle} />
-            <Enterprise icon={faDiscord } />
+                <Enterprise icon={faInstagram} />
+                <Enterprise icon={faGoogle} />
+                <Enterprise icon={faDiscord} />
             </div>
         </div>
     </>
