84cae650e4e55f8706440122d10c323f169754a3
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-06-05 21:40:20 -0300

    dadasd

--- src/pages/home_page/HomePage.js
+++ src/pages/home_page/HomePage.js
@@ -1,11 +1,16 @@
 import { FixedSizeGrid } from "react-window";
 import styles from './home.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react";
+import { faPencil, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 function HomePage() {
-  const cards = Array.from({ length: 10 }, (_, i) => ({
+  const [cards ,setCartoes] = useState{
+   Array.from({ length: 10 }, (_, i) => ({
     Id: i + 1,
     Nome: ` jacú  ${i + 1}`,
   }));
+  }
 
   const columnCount = 3;
   const rowCount = 3;
@@ -13,40 +18,74 @@ function HomePage() {
   const rowHeight = 250;
 
   const Cell = ({ columnIndex, rowIndex, style }) => {
-    const index = rowIndex * columnCount + columnIndex;
+    const index = rowIndex * 5 + columnIndex;
 
-    if (index >= cards.length) return null;
-
-    const card = cards[index];
-
-    return (
-      <div style={{ ...style, padding: "0", margin: "0", boxSizing: "border-box", borderRadius: "20px" }}>
-        <div className={styles.Card}>
-          <div className={styles.cardp}>
-            <h4>{card.Nome}</h4>
-            <p className={styles.typing}> ID: {card.Id}</p>
+    if (index - 1 >= cards.length) {
+      return null
+    }
+    else if (index >= cards.length) {
+      return (
+        <div style={{ ...style }}>
+          <div className={`${styles.Card} ${styles.AddCard}`} onClick={
+            () => {
+              const newcard= {
+                id: card.length + 1,
+                nome: `Cartão ${cartoes.length + 1}`,
+                img: "https://th.bing.com/th/id/OIP.GELKQ4d2ogvLPHMtaI4UygHaHa?rs=1&pid=ImgDetMain",
+              };
+              setCartoes([...cartoes, novoCartao]);
+            }
+          }>
+            <h1 style={{ fontSize: "5vw" }}>+</h1>
           </div>
         </div>
-      </div>
-    );
-  };
+      );
+    } else {
+      const card = cards[index];
 
-  return (
-    <FixedSizeGrid
-      style={{ overflow: 'visible' }}
-      columnCount={columnCount}
-      rowCount={rowCount}
-      columnWidth={columnWidth}
-      rowHeight={rowHeight}
-      width={columnCount * columnWidth}
-      height={rowCount * rowHeight}
-      className={styles.Fckscroll}
-      overflow='scroll'
-    >
-      {Cell}
-    </FixedSizeGrid>
-  );
-}
+      return (
+        <div style={{ ...style, padding: "0", margin: "0", boxSizing: "border-box", borderRadius: "20px" }}>
+          <div className={styles.Card}>
+            <div className={styles.cardp}>
+              <h4>{card.Nome}</h4>
+              <p className={styles.typing}> ID: {card.Id}</p>
+              <div>
+                <button className={styles.BinButton} onClick={() => {
+                  setCartoes(
+                    [
+                      ...cartoes.slice(0, index),
+                      ...cartoes.slice(index + 1)
+                    ]
+                  );
+                }}>
+                  <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+                <button className={styles.PencilButton}>
+                  <FontAwesomeIcon icon={faPencil} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    };
+
+    return <>
+      <FixedSizeGrid
+        style={{ overflow: 'visible' }}
+        columnCount={columnCount}
+        rowCount={rowCount}
+        columnWidth={columnWidth}
+        rowHeight={rowHeight}
+        width={columnCount * columnWidth}
+        height={rowCount * rowHeight}
+        className={styles.Fckscroll}
+        overflow='scroll'
+      >
+        {Cell}
+      </FixedSizeGrid>
+    </>
+  }
 
 
-export default HomePage
+  export default HomePage
